b0771af2e01de3ca65ff9604c5e086a79414034e
Josh <jdlazar2011@gmail.com>
2018-01-23 22:12:59 -0500

    comments added to app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 //Data for name and price of store products
 var invData = [
-{name: "apples", price: 1.50}, 
+//ADD A QUANTITY PROPERTY AND RELEVANT METHODS/PARTS OF METHODS THAT QUERY INVENTORY
+//ADD AND SUBTRACT TO INVENTORY AS ITS MOVED TO AN ORDER
+//PREVENTS ADDING ITEMS IF INVENTORY === 0
+{name: "apples", price: 1.50},
 {name: "oranges", price: 1.25 },
 {name: "bananas", price: 2.10 }
 ]
 //Array of store products as objects
+
 var invArray = [];
 
 //Array of store products as objects in user's cart
@@ -17,93 +21,114 @@ function cartTotal(){
 
 	cartArray.forEach(function (cartObj){
 		var itemTotal = cartObj.quantity*cartObj.price;
-		cartTotal += parseFloat(itemTotal.toFixed(2)); 
+		cartTotal += parseFloat(itemTotal.toFixed(2));
 	})
-	cartTotalContainer.innerHTML= "$"+cartTotal.toFixed(2); 
+	cartTotalContainer.innerHTML= "$"+cartTotal.toFixed(2);
 }
 
 //Object constructor for store products
+//BROADLY, W/O DELVING TOO FAR IN, I WOULD IMAGINE THE ITEM/CART/ORDER
+//RELATIONSHIP TO LOOK SOMETHING LIKE:
+//OBJECT THAT CREATES ARRAY OF ITEM OBJECTS-instantiating this as a global, allows it to
+//be passed to other objects, so that it can be "used" by inventory display
+//passed and "used" by cart
+//passed and "used" by ORDER
+//IN THIS PARADIGM WE HAVE ONE SOURCE OF INVENTORY (THE GLOBAL INSTANTIATED)
+//WE CAN PASS IT TO CART, THEN CART RELATED FUNCTIONS CAN ADD MEMBERS  TO THEIR CART ARRAY,
+//(LOCAL TO AN INSTANTIATED CART OBJECT), QUERY MEMBERS FOR INVENTORY, CHANGE INVENTORY NUMBERS ETC
+//IT WILL STILL BE THE SAME SINGLE SOURCE.  THIS IS KIND OF THE OO PARADIGM,
+//DISCRETE ELEMENTS DETERMINED BY PURPOSE, THAT ARE PASSED TO OTHER OBJECTS SO THAT THOSE OBJECTS CAN
+//ACCESS RELEVANT PROPERTIES/METHODS
+
 function EcommItem(name, price){
 	var self = this;
-	this.name = name; 
-	this.price = price; 
-	this.quantity = 1; 
+	this.name = name;
+	this.price = price;
+	this.quantity = 1;
 
 	//adds the item from inventory to user's cart
+	//TO CONSIDER: THERE SHOULD PROBABLY BE CART AND ORDER SEPARATE FROM ONE ANOTHER
+	//CART WILL DISPLAY ITEMS, QUANTITY, PRICES
+	//CHECKOUT BUTTON WILL TOTAL UP ORDER AND DISPLAY IT
+	//EcommITem and Cart are PROB SEPARATE OBJECTS SEEMS TO ME CART METHODS DONT BELONG ON EcommItem
+	//**TO THINK ABOUT: REALLY TEASE OUT WHAT THE CART DOES WHAT THE ORDER DOES, AND WHATS "AT THE CENTER"
+	//AS AN EXERCISE WE'LL ESSENTIALLY BE PASSING THE INVENTORY OBJECT INTO OTHER OBJECTS SO THAT THEY CAN ACCESS IT
+	//TO DISPLAY INVENTORY, CART INVENTORY AND COMPLETE AN ORDER FOR INVENTORY.  OBJECT DESIGN AND INTERACTION BETWEEN
+	//SHOULD BE GUIDED BY THOSE CONSIDERATIONS
 	this.addToCart = function addToCart(){
-		var inputQuantity = document.getElementById(self.name+"-preCart-quantity"); 
+		var inputQuantity = document.getElementById(self.name+"-preCart-quantity");
 		var inputQuantityValue = parseInt(inputQuantity.value);
 		if(isNaN(inputQuantityValue)){
-	 		inputQuantityValue = 1; 
+	 		inputQuantityValue = 1;
 	 	}
-		var itemAlreadyInCart = false; 
+		var itemAlreadyInCart = false;
 
-		//updates cart quantity: if product already in user's cart, updates quantity with number entered by user within the cart display 
+		//updates cart quantity: if product already in user's cart, updates quantity with number entered by user within the cart display
 		cartArray.forEach(function (cartItem){
 			if(cartItem.name === self.name){
-				itemAlreadyInCart = true;   
+				itemAlreadyInCart = true;
 				cartItem.quantity += inputQuantityValue;
-				inputQuantity.value = "";   
+				inputQuantity.value = "";
 				renderProduct(cartArray, "cart");
-				cartTotal();  
-				return 
+				cartTotal();
+				return
 				}
 			})
 		//if product not in user's cart, adds new item to user's cart
 		if(itemAlreadyInCart === false){
-			self.quantity = inputQuantityValue; 
-			cartArray.push(self); 
-			inputQuantity.value = ""; 
+			self.quantity = inputQuantityValue;
+			cartArray.push(self);
+			inputQuantity.value = "";
 			renderProduct(cartArray, "cart");
-			cartTotal(); 
-			return 
-		}		
+			cartTotal();
+			return
+		}
 	}
-	//if user clicks on cart quantity input, resets input to blank value  
-	this.resetQuantity = function resetQuantity(){; 
+	//if user clicks on cart quantity input, resets input to blank value
+	this.resetQuantity = function resetQuantity(){;
 		var quantityContainer = document.getElementById(self.name+"-cart-quantity");
-		quantityContainer.value = ""; 
+		quantityContainer.value = "";
 	}
 	//removes item from user's cart
 	this.removeFromCart = function removeFromCart(){
 		cartArray.forEach(function (cartItem){
 			if(cartItem.name === self.name){
-				var cartItemIndex = cartArray.indexOf(cartItem); 
-				cartArray.splice(cartItemIndex, 1); 
-				renderProduct(cartArray, "cart"); 
+				var cartItemIndex = cartArray.indexOf(cartItem);
+				cartArray.splice(cartItemIndex, 1);
+				renderProduct(cartArray, "cart");
 			}
 		})
-		cartTotal(); 
+		cartTotal();
 	}
-	//updates cart quantity: takes number entered by user within the cart display 
-	this.updateQuantity = function updateQuantity(){ 
+	//updates cart quantity: takes number entered by user within the cart display
+	this.updateQuantity = function updateQuantity(){
 		var newQuantity = document.getElementById(self.name+"-cart-quantity");
-		var newQuantityValue = parseInt(newQuantity.value); 
+		var newQuantityValue = parseInt(newQuantity.value);
 
 		if(isNaN(newQuantityValue)){
-			newQuantityValue = 1; 
-			newQuantity.value = 1; 
-		} 
-		self.quantity = newQuantityValue; 
-		var totalPriceContainer = document.getElementById(self.name+"-totalPrice"); 
-		totalPriceContainer.innerHTML = "Total Price: $"+(self.price*self.quantity).toFixed(2); 
-		cartTotal(); 
+			newQuantityValue = 1;
+			newQuantity.value = 1;
+		}
+		self.quantity = newQuantityValue;
+		var totalPriceContainer = document.getElementById(self.name+"-totalPrice");
+		totalPriceContainer.innerHTML = "Total Price: $"+(self.price*self.quantity).toFixed(2);
+		cartTotal();
 	}
 }
 //Loops through objects in an array, calls an object constructor on each and pushes the new object into another array
 function createInvArray(dataArray, newArray){
 	dataArray.forEach(function (invObj){
-		var newInvItem = new EcommItem(invObj.name, invObj.price); 
+		var newInvItem = new EcommItem(invObj.name, invObj.price);
 		newArray.push(newInvItem)
 	})
 }
 
 //Loops through invData to create a new object, which is then pushed into the store's inventory array (invArray)
-createInvArray(invData, invArray); 
+createInvArray(invData, invArray);
 
 //loops through the objects in an array and creates HTML elements to display the object in the browser
 function renderProduct(arrayName, containerName){
-	var container = document.getElementById(containerName); 
+	var container = document.getElementById(containerName);
 	while (container.firstChild) {
 		container.removeChild(container.firstChild);
 	}
@@ -111,15 +136,15 @@ function renderProduct(arrayName, containerName){
 	arrayName.forEach(function (itemObj){
 		var productContainer = document.createElement("div");
 		productContainer.setAttribute("id", itemObj.name);
-		productContainer.className="product";  
-		var productTextContainer = document.createElement("p"); 
+		productContainer.className="product";
+		var productTextContainer = document.createElement("p");
 		productTextContainer.className="product-name";
-		var productName = document.createTextNode(itemObj.name); 
+		var productName = document.createTextNode(itemObj.name);
 		container.appendChild(productContainer).appendChild(productTextContainer).appendChild(productName)
 
-		var priceContainer = document.createElement("p"); 
+		var priceContainer = document.createElement("p");
 		priceContainer.className="product-price"
-		var price = itemObj.price; 
+		var price = itemObj.price;
 		var priceText = document.createTextNode("Price: $"+price.toFixed(2)+"")
 		productContainer.appendChild(priceContainer).appendChild(priceText);
 
@@ -133,42 +158,37 @@ function renderProduct(arrayName, containerName){
 		productContainer.appendChild(quantityInput);
 
 		if(containerName==="inventory"){
-			quantityInput.placeholder= 1; 
+			quantityInput.placeholder= 1;
 			quantityInput.setAttribute("id", itemObj.name+"-preCart-quantity");
-			var addToCartBtn = document.createElement("button"); 
+			var addToCartBtn = document.createElement("button");
 			var addToCartTxt = document.createTextNode("Add to Cart");
-			productContainer.appendChild(addToCartBtn).appendChild(addToCartTxt); 
+			productContainer.appendChild(addToCartBtn).appendChild(addToCartTxt);
 			addToCartBtn.addEventListener("click", itemObj.addToCart);
 
 		} else if (containerName==="cart"){
 			quantityInput.setAttribute("id", itemObj.name+"cart-quantity");
-			var totalPriceContainer = document.createElement("p"); 
-			var totalPrice = (itemObj.price*itemObj.quantity).toFixed(2); 
+			var totalPriceContainer = document.createElement("p");
+			var totalPrice = (itemObj.price*itemObj.quantity).toFixed(2);
 			var totalPriceNumber = document.createTextNode("Total Price: $"+totalPrice);
-			productContainer.appendChild(totalPriceContainer).appendChild(totalPriceNumber); 
+			productContainer.appendChild(totalPriceContainer).appendChild(totalPriceNumber);
 			totalPriceContainer.setAttribute("id", itemObj.name+"-totalPrice");
-			document.getElementById(itemObj.name+"cart-quantity").addEventListener("click", itemObj.resetQuantity); 
+			document.getElementById(itemObj.name+"cart-quantity").addEventListener("click", itemObj.resetQuantity);
 
-			var updateQuantityBtn = document.createElement("button"); 
-			var updateQuantityTxt = document.createTextNode("Update Quantity"); 
-			productContainer.appendChild(updateQuantityBtn).appendChild(updateQuantityTxt); 
+			var updateQuantityBtn = document.createElement("button");
+			var updateQuantityTxt = document.createTextNode("Update Quantity");
+			productContainer.appendChild(updateQuantityBtn).appendChild(updateQuantityTxt);
 			updateQuantityBtn.className="update-btn";
 			updateQuantityBtn.addEventListener("click", itemObj.updateQuantity)
 
-			quantityInput.setAttribute("id", itemObj.name+"-cart-quantity"); 
-			quantityInput.value = itemObj.quantity; 
-			var removeFromCartBtn = document.createElement("button"); 
+			quantityInput.setAttribute("id", itemObj.name+"-cart-quantity");
+			quantityInput.value = itemObj.quantity;
+			var removeFromCartBtn = document.createElement("button");
 			var RemoveFromCartTxt = document.createTextNode("Remove");
-			productContainer.appendChild(removeFromCartBtn).appendChild(RemoveFromCartTxt); 
-			removeFromCartBtn.addEventListener("click", itemObj.removeFromCart);	
-		}		
+			productContainer.appendChild(removeFromCartBtn).appendChild(RemoveFromCartTxt);
+			removeFromCartBtn.addEventListener("click", itemObj.removeFromCart);
+		}
 	})
 }
 
-//calls render function on array of inventory objects 
+//calls render function on array of inventory objects
 renderProduct(invArray, "inventory")
-
-
-
-
-
